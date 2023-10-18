ca848b30fb06dba913876f84dd81bc2aa1e281f9
satyaprakash2611 <147645437+satyaprakash2611@users.noreply.github.com>
2023-10-18 22:18:17 +0530

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,2 +1,11 @@
-lst = [1,2,3,4,5,6,7,8,9]
 #Write a command to take above list as an input and find in the squares of a number and return a list
+
+lst = [1,2,3,4,5,6,7,8,9]
+lst2 = [x**2 for x in lst]
+print(lst2)
+
+#ye wala to easy tha...ek pehle v try kiye the 
+#dictionary m merge in kr pa rhe h to google kiye to function mila ek 'zip' wo try kiye h
+
+lst3 = dict(zip(lst,lst2))
+print(lst3)
